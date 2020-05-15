9c77dd0e28d0f033fa2aae1b1da56e719a6ee26f
sanjeev0007 <sonisanjeev650@gmail.com>
2020-05-15 11:20:08 +0530

    Day 13 	Day 14

---
+++ Week2/remove_k_digit.cpp
@@ -0,0 +1,54 @@
+/*
+
+Problem Statement!!!
+
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+    The length of num is less than 10002 and will be ≥ k.
+    The given num does not contain any leading zero.
+
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+Example 3:
+
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+*/
+
+class Solution
+{
+public:
+    string removeKdigits(string num, int k)
+    {
+        int n = num.size(), remain = n - k;
+        if (remain == 0)
+            return "0";
+        string s = "";
+        for (auto x : num)
+        {
+            while (n > remain && !s.empty() && s.back() - '0' > x - '0')
+            {
+                s.pop_back();
+                n--;
+            }
+            s.push_back(x);
+        }
+        int i = 0;
+        while (i < s.size() && s[i] == '0')
+            i++;
+        return s.substr(i, remain) == "" ? "0" : s.substr(i, remain);
+    }
+};
