a299b8da3c5d988c0a7c23e0611d95cb9b7d747c
Sanjeev Soni <44490954+sanjeev0007@users.noreply.github.com>
2020-05-26 12:43:10 +0530

    Day 26

--- Week4/contiguous_array.cpp
+++ Week4/contiguous_array.cpp
@@ -8,16 +8,15 @@ Example 1:
 
 Input: [0,1]
 Output: 2
+
 Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
 
 Example 2:
 
 Input: [0,1,0]
 Output: 2
+
 Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
-
-
-
 
 */
 
